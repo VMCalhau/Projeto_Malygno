4963fe79e48f40c0fdebdd77cb3fac14ad373355
Vítor de Melo Calhau <42739346+VMCalhau@users.noreply.github.com>
2018-09-18 07:53:33 -0300

    Add files via upload

---
+++ Programa.java
@@ -0,0 +1,47 @@
+import java.io.*;
+
+public class Programa
+{
+	public static void main (String[] args)
+	{
+		try
+		{
+			int linhas = 0, colunas = 0;
+			char labirinto[][];
+
+			BufferedReader teclado = new BufferedReader (new InputStreamReader(System.in));
+
+			String arq;
+
+			System.out.println("Digite o nome do arquivo (sem extensão): ");
+			arq = teclado.readLine();
+
+			arq += ".txt";
+
+			/*
+			if (arq.substring(arq.length()-4, 4) != ".txt")
+			{
+				arq += ".txt";
+			}
+			else {}
+			*/
+
+			BufferedReader arquivo = new BufferedReader (new FileReader (arq));
+
+			for (String linha = arquivo.readLine(); linha != null; linha = arquivo.readLine())
+			{
+				if (linhas == 0)
+					linhas = Integer.parseInt(linha);
+				else if (colunas == 0)
+					colunas = Integer.parseInt(linha);
+     		}
+
+     		labirinto = new char[linhas][colunas];
+
+		}
+		catch (Exception erro)
+		{
+			System.err.println (erro.getMessage());
+		}
+	}
+}
